e9af6ec8dbbb55cd7698f90f743d251652e58d98
gilmar jose <gilmarjose2014@gmail.com>
2023-06-07 17:21:52 -0300

    projeto completo

---
+++ src/components/Input/index.js
@@ -0,0 +1,12 @@
+import { InputContainer } from "./styles";
+const Input = ({ value }) => {
+  return (
+    <div className="App">
+    <InputContainer>
+		  <input disabled value={value}/>
+		</InputContainer>
+    </div>
+  );
+}
+
+export default Input;
